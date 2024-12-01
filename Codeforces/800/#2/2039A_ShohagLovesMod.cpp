4aa4e4a32dff405ea4045f90270e563fc1d787af
Ognjen <0gnjen1@protonmail.com>
2024-12-01 23:45:29 +0100

    Problem: 2039A - Shohag Loves Mod

---
+++ Codeforces/800/#2/2039A_ShohagLovesMod.cpp
@@ -0,0 +1,30 @@
+/*
+	    Github: 0gnjen1
+	Codeforces: 0gnjen1
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template<class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int main(){
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);	
+
+	int t; cin >> t;
+	while(t--){
+
+		int n; cin >> n;
+		for(int i=1; i<=(2*n); i+=2) cout << i << ' ';
+		cout << '\n';
+	
+	}
+
+}
+
