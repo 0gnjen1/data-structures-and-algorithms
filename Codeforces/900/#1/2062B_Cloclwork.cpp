bda83463158b1670515220f373779695fc01b2d8
Ognjen <0gnjen1@protonmail.com>
2025-02-26 16:46:54 +0100

    Problem: 2062B - Clockwork

---
+++ Codeforces/900/#1/2062B_Cloclwork.cpp
@@ -0,0 +1,38 @@
+/*
+	    Github: 0gnjen1
+	Codeforces: 0gnjen1
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma GCC target("avx,avx2,fma")
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template<class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+//	.find_by_order(k)	returns an iterator to the kth smallest element (counting from 0) in O(logN)
+//	.order_of_key(k)	returns the number of items that are strictly smaller than k in O(logN)
+
+int main(){
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);	
+
+	int t; cin >> t;
+	while(t--){
+
+		int n; cin >> n;
+		int ok = true;
+		for(int i=1; i<=n; i++){
+			int clock; cin >> clock;
+			if(clock<=2*max(i-1, n-i)) ok = false;
+		}
+		cout << (ok ? "YES\n" : "NO\n");		
+
+	}
+
+}
+
