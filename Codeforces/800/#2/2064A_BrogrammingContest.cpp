8a30b1ba7bc31007a17885c6f6e894f2ccd4cb0a
Ognjen <0gnjen1@protonmail.com>
2025-03-03 20:21:21 +0100

    Problem: 2064A - Brogramming Contest

---
+++ Codeforces/800/#2/2064A_BrogrammingContest.cpp
@@ -0,0 +1,32 @@
+/*
+	    Github: 0gnjen1
+	Codeforces: 0gnjen1
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template<class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int main(){
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);	
+
+	int t; cin >> t;
+	while(t--){
+
+		int n; cin >> n;
+		string s; cin >> s;
+		int ans = (s[0]=='1' ? 1 : 0); 
+		for(int i=1; i<n; i++) if(s[i]!=s[i-1]) ans++;
+		cout << ans << '\n';
+
+	}
+
+}
+
